040915164e0df942bf717fbd29913efdfa3d28c7
Bhaviraj Singh <bhaviraj439@gmail.com>
2026-02-22 00:16:03 +0530

    update server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use("/api/languages", languageRoutes);
 
 // Base route
 app.get("/", (req, res) => {
-  res.json({ message: "Indian Languages API is running 🚀" });
+  res.json({ message: "Indian Languages API is running" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
